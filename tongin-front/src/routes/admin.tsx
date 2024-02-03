c194610b3339c2a2f405608422fa65a061ddba83
Sunghoon Lee <leesh940312@gmail.com>
2024-02-03 23:15:02 +0900

    fix

--- tongin-front/src/routes/admin.tsx
+++ tongin-front/src/routes/admin.tsx
@@ -1,6 +1,16 @@
+import styled from "styled-components";
+import AdminHeader from "../css/admin/admin";
+
+// const Header = styled.div`
+//   width: 400px;
+//   height: 100px;
+//   background-color: red;
+// `;
+
 export default function Admin() {
   return (
     <>
+      <AdminHeader>헤더영역</AdminHeader>
       <h1>관리자페이지</h1>
     </>
   );
